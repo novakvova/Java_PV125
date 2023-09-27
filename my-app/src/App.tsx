75b276f99927bc0b1737289f0538d0c932ba115c
novakvova <novakvova@gmail>
2023-09-27 20:51:50 +0300

    Working admin panel begin

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,29 +1,70 @@
 import './App.css'
+import { Suspense, lazy, useEffect, useState } from 'react';
 import {Route, Routes} from "react-router-dom";
 import DefaultLayout from "./components/containers/default/DefaultLayout.tsx";
 import CategoryListPage from "./components/category/list/CategoryListPage.tsx";
 import CategoryCreatePage from "./components/category/create/CategoryCreatePage.tsx";
 import ProductCreatePage from "./components/product/create/ProductCreatePage.tsx";
 import Login from "./components/auth/login";
+import { Toaster } from 'react-hot-toast';
 
+import ECommerce from './pages/Dashboard/ECommerce';
+import SignIn from './pages/Authentication/SignIn';
+import SignUp from './pages/Authentication/SignUp';
+import Loader from './common/Loader';
+import routes from './routes';
+
+const AdminLayout = lazy(() => import('./layout/AdminLayout.tsx'));
 
 function App() {
 
+    const [loading, setLoading] = useState<boolean>(true);
 
-  return (
-    <>
-        <Routes>
-            <Route path={"/"} element={<DefaultLayout/>}>
-                <Route index element={<CategoryListPage />} />
-                <Route path={"create"} element={<CategoryCreatePage />} />
-                <Route path={"product"}>
-                    <Route path={"create"} element={<ProductCreatePage/>} />
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 1000);
+    }, []);
+
+
+    return loading ? (
+        <Loader />
+    ) : (
+        <>
+            <Toaster position='top-right' reverseOrder={false} containerClassName='overflow-auto'/>
+
+            <Routes>
+                <Route path="/auth/signin" element={<SignIn />} />
+                <Route path="/auth/signup" element={<SignUp />} />
+                <Route element={<AdminLayout />}>
+                    <Route index element={<ECommerce />} />
+                    {routes.map(({ path, component: Component }) => (
+                        <Route
+                            path={path}
+                            element={
+                                <Suspense fallback={<Loader />}>
+                                    <Component />
+                                </Suspense>
+                            }
+                        />
+                    ))}
                 </Route>
-                <Route path={"login"} element={<Login />}></Route>
-            </Route>
-        </Routes>
-    </>
-  )
+            </Routes>
+        </>
+    );
+
+  // return (
+  //   <>
+  //       <Routes>
+  //           <Route path={"/"} element={<DefaultLayout/>}>
+  //               <Route index element={<CategoryListPage />} />
+  //               <Route path={"create"} element={<CategoryCreatePage />} />
+  //               <Route path={"product"}>
+  //                   <Route path={"create"} element={<ProductCreatePage/>} />
+  //               </Route>
+  //               <Route path={"login"} element={<Login />}></Route>
+  //           </Route>
+  //       </Routes>
+  //   </>
+  // )
 }
 
 export default App
--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -4,6 +4,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './satoshi.css';
 import {BrowserRouter} from "react-router-dom";
 import 'flowbite';
 import {Provider} from "react-redux";
